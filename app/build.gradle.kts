854032d1c708fc545d72d017b9e7fd48bc048bb2
helsk8 <118549978+helsk8@users.noreply.github.com>
2024-09-19 18:14:09 -0500

    Menu sin estilo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.tareatracker"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.tareatracker"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
