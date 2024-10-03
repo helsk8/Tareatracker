5ea7d82a68716f6a1376a2371e0d0ade64d39c3a
helsk8 <helsk8@yahoo.com.mx>
2024-10-03 09:12:04 -0500

    Vista actualizada y funcionando

---
+++ app/src/main/java/com/example/tareatracker/ui/TaskAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.tareatracker.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tareatracker.R
+
+class TaskAdapter(private val taskList: List<Task>) : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val taskName: TextView = itemView.findViewById(R.id.task_name_text)
+        val taskDescription: TextView = itemView.findViewById(R.id.task_description_text)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.task_item, parent, false)
+        return TaskViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val currentTask = taskList[position]
+        holder.taskName.text = currentTask.name
+        holder.taskDescription.text = currentTask.description
+    }
+
+    override fun getItemCount(): Int {
+        return taskList.size
+    }
+}
